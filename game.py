5c6b295d6772ebb88a19304ceb89656d68bb7053
Dioni00 <dsanbla@gmail.com>
2021-02-22 12:37:40 +0100

    Third stage

--- game.py
+++ game.py
@@ -1,49 +1,26 @@
-# Write your code here
-import random
+from random import choice, seed
 
-options_gamer = ["rock", "paper", "scissors"]
-options_computer = ["rock", "paper", "scissors"]
+options = ['rock', 'paper', 'scissors']
 
-# Check results. To improve in following stages with dictionary
-def results(gamer, computer):
-    if gamer == computer:
-        print(f"There is a draw ({options_gamer[gamer]})")
-    elif (gamer == 0 and computer == 2) or \
-            (gamer == 1 and computer == 0) or \
-            (gamer == 2 and computer == 1):
-        print(f"Well done. The computer chose {options_computer[computer]} and failed")
-    else:
-        print(f"Sorry, but the computer chose {options_computer[computer]}")
+player = input()
 
-# Random chose for Computer
-cp_choice = random.choice(options_computer)
+while player != "!exit":
+  beats = {'rock':'scissors', 'scissors':'paper', 'paper':'rock'}
 
-# Option from gamer
-option = input()
+  cpu = choice(options)
+  
+  if player not in options:
+    print("Invalid input")
 
-# Get index to compare lists
-ind_gamer = options_gamer.index(option)
-ind_cpu = options_computer.index(cp_choice)
+  elif player == cpu:
+    print(f"There is a draw ({cpu})")
 
-results(ind_gamer, ind_cpu)
+  elif beats[player] == cpu:
+    print(f'Well done. Computer chose {cpu} and failed')
 
-#Another solution
-'''
-from random import choice
+  elif beats[cpu] == player:
+    print(f'Sorry, but computer chose {cpu}')
 
-cpu=choice(['rock','paper','scissors'])
-print(cpu)
-player=input()
+  player = input()
 
-beats={'rock':'scissors','scissors':'paper','paper':'rock'}
-
-if player==cpu:
-  print(f"There is a draw ({cpu})")
-
-elif beats[player]==cpu:
-  print(f'Well done. Computer chose {cpu} and failed')
-
-elif beats[cpu]==player:
-  print(f'Sorry, but computer chose {cpu}')
-
-'''
+print("Bye!")
